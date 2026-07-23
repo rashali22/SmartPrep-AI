08841a6f69ee183fa93e27ddfe512e96f73f3f32
rashali22 <rashalibajaj22@gmail.com>
2026-07-23 15:57:35 +0530

    Fix Puppeteer for Render deployment

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -108,7 +108,13 @@ Generate a detailed interview preparation report.
 }
 
 async function generatePdfFromHtml(htmlContent) {
-    const browser = await puppeteer.launch();
+   const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+    ],
+});
 
     const page = await browser.newPage();
 
